69abcc2a008378cea17e4fd94e90e9f3843bea38
Zhamaliyev <1994-23@mail.ru>
2018-12-07 16:19:15 +0600

    Version 1.2.0

--- src/Facade.php
+++ src/Facade.php
@@ -10,18 +10,20 @@
 
 namespace Paybox\Payout;
 
-use Paybox\Core\Exceptions\Property as PropertyException;
-use Paybox\Core\Exceptions\Connection as ConnectionException;
-use Paybox\Core\Exceptions\Request as RequestException;
-use Paybox\Core\Abstractions\DataContainer;
-use Paybox\Core\Interfaces\Payout as PayoutInterface;
+use Paybox\Core\ {
+    Exceptions\Property as PropertyException,
+    Exceptions\Connection as ConnectionException,
+    Exceptions\Request as RequestException,
+    Abstractions\DataContainer,
+    Interfaces\Payout as PayoutInterface
+};
 
 /**
  * Facade of Paybox\Payout classes
  * Simple facade for comfortable using a whole Paybox Payout functionality
  *
  * @package Paybox\Payout
- * @version 1.1.0
+ * @version 1.2.0
  * @copyright LLC Paybox.money
  * @license GPLv3 @link https://www.gnu.org/licenses/gpl-3.0-standalone.html
  *
@@ -57,7 +59,7 @@ class Facade extends DataContainer implements PayoutInterface {
      *
      */
 
-    public function reg2reg() {
+    public function reg2reg():bool {
         try {
             $this->order->required('amount');
             $this->order->required('description');
@@ -69,11 +71,7 @@ class Facade extends DataContainer implements PayoutInterface {
             $this->send();
             $this->redirectUrl = $this->getServerAnswer('redirect_url');
             return true;
-        } catch(PropertyException $e) {
-            echo $e->getMessage();
-        } catch(ConnectionException $e) {
-            echo $e->getMessage();
-        } catch(RequestException $e) {
+        } catch(PropertyException | ConnectionException | RequestException $e) {
             echo $e->getMessage();
         }
 
@@ -88,7 +86,7 @@ class Facade extends DataContainer implements PayoutInterface {
      *
      */
 
-    public function reg2nonreg() {
+    public function reg2nonreg():bool {
         try {
             $this->order->required('amount');
             $this->order->required('description');
@@ -99,11 +97,7 @@ class Facade extends DataContainer implements PayoutInterface {
             $this->send();
             $this->redirectUrl = $this->getServerAnswer('redirect_url');
             return true;
-        } catch(PropertyException $e) {
-            echo $e->getMessage();
-        } catch(ConnectionException $e) {
-            echo $e->getMessage();
-        } catch(RequestException $e) {
+        } catch(PropertyException | ConnectionException | RequestException $e) {
             echo $e->getMessage();
         }
 
@@ -118,7 +112,7 @@ class Facade extends DataContainer implements PayoutInterface {
      *
      */
 
-    public function toIban() {
+    public function toIban():bool {
         try {
             $this->order->required('amount');
             $this->order->required('description');
@@ -126,11 +120,7 @@ class Facade extends DataContainer implements PayoutInterface {
             $this->send();
             $this->redirectUrl = $this->getServerAnswer('redirect_url');
             return true;
-        } catch(PropertyException $e) {
-            echo $e->getMessage();
-        } catch(ConnectionException $e) {
-            echo $e->getMessage();
-        } catch(RequestException $e) {
+        } catch(PropertyException | ConnectionException | RequestException $e) {
             echo $e->getMessage();
         }
 
@@ -150,18 +140,15 @@ class Facade extends DataContainer implements PayoutInterface {
      *
      */
 
-    public function getIbanStatus() {
+    public function getIbanStatus():string {
         try {
             $this->order->required('id');
             $this->save('api/iban_payout/payment_status', false);
             $this->send();
             return $this->getServerAnswer('status');
-        } catch(PropertyException $e) {
+        } catch(PropertyException | ConnectionException | RequestException $e) {
             echo $e->getMessage();
-        } catch(ConnectionException $e) {
-            echo $e->getMessage();
-        } catch(RequestException $e) {
-            echo $e->getMessage();
+            return false;
         }
     }
 
@@ -178,18 +165,15 @@ class Facade extends DataContainer implements PayoutInterface {
      *
      */
 
-    public function getStatus() {
+    public function getStatus():string {
         try {
             $this->order->required('id');
             $this->save('api/payment_status', false);
             $this->send();
             return $this->getServerAnswer('status');
-        } catch(PropertyException $e) {
+        } catch(PropertyException | ConnectionException | RequestException $e) {
             echo $e->getMessage();
-        } catch(ConnectionException $e) {
-            echo $e->getMessage();
-        } catch(RequestException $e) {
-            echo $e->getMessage();
+            return false;
         }
     }
 
@@ -200,17 +184,14 @@ class Facade extends DataContainer implements PayoutInterface {
      *
      */
 
-    public function getBalance() {
+    public function getBalance():string {
         try {
             $this->save('api/balance_status', false);
             $this->send();
             return $this->getServerAnswer('status');
-        } catch(PropertyException $e) {
+        } catch(PropertyException | ConnectionException | RequestException $e) {
             echo $e->getMessage();
-        } catch(ConnectionException $e) {
-            echo $e->getMessage();
-        } catch(RequestException $e) {
-            echo $e->getMessage();
+            return false;
         }
     }
 
@@ -224,7 +205,7 @@ class Facade extends DataContainer implements PayoutInterface {
      *
      */
 
-    public function accept($successMessage) {
+    public function accept(string $successMessage):string {
         header('Content-Type: application/xml', true, 200);
         $this->answer->status = 'ok';
         $this->answer->description = $successMessage;
@@ -241,7 +222,7 @@ class Facade extends DataContainer implements PayoutInterface {
      * @return array
      */
 
-    public function parseXML($request) {
+    public function parseXML($request):array {
         return (array) (new \SimpleXMLElement($request['pg_xml']));
     }
 
@@ -253,7 +234,7 @@ class Facade extends DataContainer implements PayoutInterface {
      *
      */
 
-    protected function getBaseUrl() {
+    protected function getBaseUrl():string {
         return 'https://api.paybox.money/';
     }
 
@@ -264,7 +245,7 @@ class Facade extends DataContainer implements PayoutInterface {
      *
      */
 
-    private function answer() {
+    private function answer():string {
         $this->toXML();
         return $this->xml->asXML();
     }
